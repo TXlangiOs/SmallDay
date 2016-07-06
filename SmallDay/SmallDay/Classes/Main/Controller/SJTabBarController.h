d13c4154cc1ddd25fd0941d54cfe50db7ea9accd
vision <vision@visionmacbook-pro.local>
2016-07-06 13:35:35 +0800

    自定义TabBar控制器

---
+++ SmallDay/SmallDay/Classes/Main/Controller/SJTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SJTabBarController.h
+//  SmallDay
+//
+//  Created by Vision on 16/7/6.
+//  Copyright © 2016年 Vision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJTabBarController : UITabBarController
+
+@end
